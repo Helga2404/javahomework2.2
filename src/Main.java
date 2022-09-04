918a32226fa337d69c16eeb5d258cc6531f001f4
Helga2404 <110912083+Helga2404@users.noreply.github.com>
2022-09-04 18:21:21 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,21 @@ public class Main {
 
         int amount = 100; //изначально на счете
         int payment = 1100; //размер платежа
-        int x = 1000; //условие бонуса
+        int minPayment = 1000; //условие бонуса
         int bonus = 1; //размер бонуса за 100 рублей
         int bonusTotal = payment / 100; //размер бонуса
         int total = amount + payment + bonusTotal; //всего на счете
 
-        if (payment >= x) {
-            System.out.println(" Всего бонусов " + bonusTotal + 
-                    " Всего на счете " + total);
+        if (payment >= minPayment) {
+            System.out.println("Всего бонусов: " + bonusTotal +
+                    " Всего на счете: " + total);
         } else {
-            System.out.println(bonus = 0);
+            System.out.println("Нет бонусов " +
+                    " Всего на счете: " + amount + payment);
 
         }
-
-        }
-
-
 
     }
 
+}
 
